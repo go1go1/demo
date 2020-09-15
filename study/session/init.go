7ca7b672594374a709cf96697a51f844f2fdb9fd
linyyyang <linyyyang@tencent.com>
2020-09-15 10:06:30 +0800

    session

---
+++ study/session/init.go
@@ -0,0 +1,22 @@
+package session
+
+import "fmt"
+
+var (
+	sessionMgr SessionMgr
+)
+
+func Init(provider string, addr string, options ...string) (err error) {
+	switch provider {
+	case "memory":
+		sessionMgr = NewMemorySessionMgr()
+	case "redis":
+		sessionMgr = NewRedisSessionMgr()
+	default:
+		err = fmt.Errorf("not support")
+		return
+	}
+
+	err = sessionMgr.Init(addr, options...)
+	return
+}
